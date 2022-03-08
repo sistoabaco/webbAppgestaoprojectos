7688755fbddd456e11bdbfc793505e7b04848459
sisto abaco <sistodacruzabaco@hotmail.com>
2022-03-08 10:35:56 +0200

    funcao gestor financeiro implementada

--- src/main/java/com/webbappgestaoprojectos/security/DataLoader.java
+++ src/main/java/com/webbappgestaoprojectos/security/DataLoader.java
@@ -25,30 +25,28 @@ public class DataLoader implements CommandLineRunner{
 
     @Override
     public void run(String...args) throws Exception {
-        pr.save(new Permissao("USER"));
         pr.save(new Permissao("ADMIN"));
         pr.save(new Permissao("P_PARCEIRO"));
-//        pr.save(new Permissao("P_TEAM LEADER"));
-//        pr.save(new Permissao("P_COORDENADOR"));
-//        pr.save(new Permissao("P_OFICIAL DE MEAL"));
-//        pr.save(new Permissao("P_GESTOR DE PROJECTOS"));
-//        pr.save(new Permissao("P_DIRECTOR"));
+        pr.save(new Permissao("P_TEAM LEADER"));
+        pr.save(new Permissao("P_COORDENADOR"));
+        pr.save(new Permissao("P_OFICIAL DE MEAL"));
+        pr.save(new Permissao("P_GESTOR DE PROJECTOS"));
+        pr.save(new Permissao("P_DIRECTOR"));
         pr.save(new Permissao("P_GESTOR FINANCEIRO"));
-//        pr.save(new Permissao("P_CONTABILISTA"));
-//        pr.save(new Permissao("P_OFICIAL DE LOGISTICA"));
-//        pr.save(new Permissao("P_ASSISTENTE DE LOGISTICA"));
-//        pr.save(new Permissao("P_SECRETARIA"));
-//        pr.save(new Permissao("P_FIEL DE ARMAZEM"));
-//        pr.save(new Permissao("P_TECNICOS DO CAMPO"));
+        pr.save(new Permissao("P_CONTABILISTA"));
+        pr.save(new Permissao("P_OFICIAL DE LOGISTICA"));
+        pr.save(new Permissao("P_ASSISTENTE DE LOGISTICA"));
+        pr.save(new Permissao("P_SECRETARIA"));
+        pr.save(new Permissao("P_FIEL DE ARMAZEM"));
+        pr.save(new Permissao("P_TECNICOS DO CAMPO"));
 
         Permissao adminPermissao = pr.findByRole("ADMIN");
-        Permissao userPermissao = pr.findByRole("USER");
         Permissao parPermissao = pr.findByRole("P_PARCEIRO");
         Permissao financeiroP = pr.findByRole("P_GESTOR FINANCEIRO");
 
         Utilizador utilizador =
                 new Utilizador("sabaco@unilurio.ac.mz", passwordEncoder.encode("12345"));
-        utilizador.setPermissao(Arrays.asList(adminPermissao, userPermissao, financeiroP,parPermissao ));
+        utilizador.setPermissao(Arrays.asList(adminPermissao, financeiroP,parPermissao ));
         utilizadorRepository.save(utilizador);
 
 //        Utilizador u = new Utilizador("pmarques@unilurio.ac.mz", passwordEncoder.encode("54321"));
@@ -56,7 +54,7 @@ public class DataLoader implements CommandLineRunner{
 //        utilizadorRepository.save(u);
 //
         projR.save(new Projecto("BHA", "Activo", 12000000.0, "Chiure e Ancuabe",
-                "01/09/2020", "02/02/2022", "USD",8276626.0));
+                "2020-08-01", "2020-10-01", "USD",8276626.0));
 
         Utilizador uPar = new Utilizador("usaid@gmail.com", passwordEncoder.encode("zxcvb"));
         uPar.setPermissao(Arrays.asList(parPermissao));
@@ -74,26 +72,15 @@ public class DataLoader implements CommandLineRunner{
          cr.save (new Categoria("TEAM LEADER", 30000.0));
          cr.save(new Categoria("COORDENADOR", 80000.0));
          cr.save(new Categoria("OFICIAL DE MEAL", 30000.0));
-
-//        cr.save(c3);
-//        Categoria c4 = new Categoria("GESTOR DE PROJECTOS", 150000.0);
-//        cr.save(c4);
-//        Categoria c5 = new Categoria("DIRECTOR", 200000.0);
-//        cr.save(c5);
-//        Categoria c6 = new Categoria("GESTOR FINANCEIRO", 135000.0);
-//        cr.save(c6);
+         cr.save(new Categoria("GESTOR FINANCEIRO", 135000.0));
+//       cr.sava( new Categoria("GESTOR DE PROGRAMAS", 150000.0));
+         cr.save(new Categoria("DIRECTOR", 200000.0));
 //        Categoria c7 = new Categoria("CONTABILISTA", 30000.0);
-//        cr.save(c7);
 //        Categoria c8 = new Categoria("OFICIAL DE LOGISTICA", 50000.0);
-//        cr.save(c8);
 //        Categoria c9 = new Categoria("ASSISTENTE DE LOGISTICA", 30000.0);
-//        cr.save(c9);
 //        Categoria c10 = new Categoria("SECRETARIA", 30000.0);
-//        cr.save(c10);
 //        Categoria c11 = new Categoria("FIEL DE ARMAZEM", 30000.0);
-//        cr.save(c11);
 //        Categoria c12 = new Categoria("OUTROS", 50000.0);
-//        cr.save(c12);
     }
 
 }
